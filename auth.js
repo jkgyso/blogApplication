83ba84acffab81348ff25c87da69f1cd7fc7c394
jkgyso <jkgyso@gmail.com>
2024-07-14 12:55:07 +0800

    auth revision

--- auth.js
+++ auth.js
@@ -16,13 +16,13 @@ module.exports.verify = (req, res, next) => {
     let token = req.headers.authorization;
 
     if (typeof token === "undefined") {
-        return res.send({ auth: "Failed. No Token" });
+        return res.status(401).send({ auth: "Failed. No Token" });
     } else {
         token = token.slice(7, token.length);
 
         jwt.verify(token, secret, function (err, decodedToken) {
             if (err) {
-                return res.status(404)send({
+                return res.status(404).send({
                     auth: "Failed",
                     message: err.message
                 });
@@ -40,7 +40,7 @@ module.exports.verifyAdmin = (req, res, next) => {
     if (req.user.isAdmin) {
         next();
     } else {
-        return res.send({
+        return res.statis(403).send({
             auth: false,
             message: 'Action Forbidden',
         });
